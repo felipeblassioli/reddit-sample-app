275bbd0e02ba28596e09e3ffcbc82d06b6e84725
Felipe <felipeblassioli@gmail.com>
2017-05-14 12:13:20 -0300

    Widget RedditLink

--- app/src/main/java/org/samples/blassioli/reddit/features/posts/PostsAdapter.java
+++ app/src/main/java/org/samples/blassioli/reddit/features/posts/PostsAdapter.java
@@ -1,32 +1,15 @@
 package org.samples.blassioli.reddit.features.posts;
 
 import android.app.Activity;
-import android.content.Intent;
-import android.os.Bundle;
-import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
-import android.text.TextUtils;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.webkit.URLUtil;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
-import com.squareup.picasso.Picasso;
-
-import org.samples.blassioli.reddit.R;
-import org.samples.blassioli.reddit.features.details.DetailsActivity;
-import org.samples.blassioli.reddit.picasso.RoundedTransformation;
 import org.samples.blassioli.reddit.widgets.RecyclerViewListAdapter;
-import org.samples.blassioli.reddit.widgets.RedditHeadline;
+import org.samples.blassioli.reddit.widgets.RedditLink;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import butterknife.BindView;
-import butterknife.ButterKnife;
 
 public class PostsAdapter extends RecyclerViewListAdapter<Post> {
     private final Activity activity;
@@ -42,90 +25,24 @@ public class PostsAdapter extends RecyclerViewListAdapter<Post> {
 
     @Override
     public RecyclerView.ViewHolder getDataViewHolder(ViewGroup parent) {
-        View v = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.post_item_card, parent, false);
-        ViewHolder vh = new ViewHolder(v);
-        return vh;
+        View v = new RedditLink(parent.getContext(), activity);
+        RecyclerView.LayoutParams lp = new RecyclerView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        v.setLayoutParams(lp);
+        return new ViewHolder(v);
     }
 
     @Override
     protected void onBindDataViewHolder(RecyclerView.ViewHolder rawHolder, int position) {
         ViewHolder holder = (ViewHolder) rawHolder;
-        Post p = getItems().get(position);
-
-        holder.cardView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(activity, DetailsActivity.class);
-                Bundle b = new Bundle();
-                b.putString(DetailsActivity.P_LINK_ID, p.id);
-                intent.putExtras(b);
-                activity.startActivity(intent);
-                activity.overridePendingTransition(R.anim.slide_up, R.anim.no_change);
-            }
-        });
-
-        holder.title.setText(p.title);
-        holder.headline.setAuthor(p.author);
-        holder.headline.setCreatedUtc(p.createdUtc);
-
-        if (p.thumbnail != null &&
-                !TextUtils.isEmpty(p.thumbnail) &&
-                URLUtil.isValidUrl(p.thumbnail)) {
-            holder.thumbnail.setVisibility(View.VISIBLE);
-            Picasso.with(activity)
-                    .load(p.thumbnail)
-                    .fit()
-                    .centerCrop()
-                    .transform(new RoundedTransformation(16, 16))
-                    .into(holder.thumbnail);
-            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
-                    LinearLayout.LayoutParams.MATCH_PARENT,
-                    LinearLayout.LayoutParams.MATCH_PARENT);
-            params.weight = .3f;
-            holder.contentGroup.setLayoutParams(params);
-        } else {
-            holder.thumbnail.setVisibility(View.GONE);
-            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
-                    LinearLayout.LayoutParams.MATCH_PARENT,
-                    LinearLayout.LayoutParams.MATCH_PARENT);
-            params.weight = 0f;
-            holder.contentGroup.setLayoutParams(params);
-        }
-        holder.body.setText(p.selftext);
-        holder.ups.setText(p.ups);
-        holder.totalComments.setText(p.numComments);
+        holder.link.setData(getItems().get(position));
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
-        @BindView(R.id.content_layout)
-        public ViewGroup contentGroup;
-
-        @BindView(R.id.headline)
-        public RedditHeadline headline;
-
-        @BindView(R.id.title_text)
-        public TextView title;
-
-        @BindView(R.id.img_thumbnail)
-        public ImageView thumbnail;
-
-        @BindView(R.id.body_text)
-        public TextView body;
-
-        @BindView(R.id.ups)
-        public TextView ups;
-
-        @BindView(R.id.total_comments)
-        public TextView totalComments;
-
-        @BindView(R.id.card_view)
-        public CardView cardView;
+        public RedditLink link;
 
         public ViewHolder(View view) {
             super(view);
-            ButterKnife.bind(this, view);
+            this.link = (RedditLink) view;
         }
     }
-
 }
---
+++ app/src/main/java/org/samples/blassioli/reddit/widgets/RedditLink.java
@@ -0,0 +1,114 @@
+package org.samples.blassioli.reddit.widgets;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.CardView;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.URLUtil;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import org.samples.blassioli.reddit.R;
+import org.samples.blassioli.reddit.features.details.DetailsActivity;
+import org.samples.blassioli.reddit.features.posts.Post;
+import org.samples.blassioli.reddit.picasso.RoundedTransformation;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class RedditLink extends CardView {
+    private final Activity activity;
+
+    @BindView(R.id.content_layout)
+    public ViewGroup contentGroup;
+
+    @BindView(R.id.headline)
+    public RedditHeadline headline;
+
+    @BindView(R.id.title_text)
+    public TextView title;
+
+    @BindView(R.id.img_thumbnail)
+    public ImageView thumbnail;
+
+    @BindView(R.id.body_text)
+    public TextView body;
+
+    @BindView(R.id.ups)
+    public TextView ups;
+
+    @BindView(R.id.total_comments)
+    public TextView totalComments;
+
+    @BindView(R.id.card_view)
+    public CardView cardView;
+
+    public RedditLink(Context context, Activity activity) {
+        super(context);
+        this.activity = activity;
+        init(context);
+    }
+
+    public void init(Context context) {
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        inflater.inflate(R.layout.post_item_card, this, true);
+        ButterKnife.bind(this);
+    }
+
+    public void setThumbnail(String thumbnailUrl) {
+        Context context = getContext();
+
+        if (thumbnailUrl != null &&
+                !TextUtils.isEmpty(thumbnailUrl) &&
+                URLUtil.isValidUrl(thumbnailUrl)) {
+            thumbnail.setVisibility(View.VISIBLE);
+            Picasso.with(context)
+                    .load(thumbnailUrl)
+                    .fit()
+                    .centerCrop()
+                    .transform(new RoundedTransformation(16, 16))
+                    .into(thumbnail);
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.MATCH_PARENT);
+            params.weight = .3f;
+            contentGroup.setLayoutParams(params);
+        } else {
+            thumbnail.setVisibility(View.GONE);
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.MATCH_PARENT);
+            params.weight = 0f;
+            contentGroup.setLayoutParams(params);
+        }
+    }
+
+    public void setData(Post p) {
+       cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = DetailsActivity.createIntent(activity, p.id);
+                activity.startActivity(intent);
+                activity.overridePendingTransition(R.anim.slide_up, R.anim.no_change);
+            }
+        });
+
+        title.setText(p.title);
+        headline.setAuthor(p.author);
+        headline.setCreatedUtc(p.createdUtc);
+
+        setThumbnail(p.thumbnail);
+
+        body.setText(p.selftext);
+        ups.setText(p.ups);
+        totalComments.setText(p.numComments);
+    }
+}
